8c8a6280f5ce30c31b4fd215d7bbc43cd38cef9a
IvanovvAlex <alexgeorgievivanov2003@gmail.com>
2020-12-18 07:06:52 +0200

    Add: First Steps In Coding- Lab

---
+++ Programming Basics/First steps in coding- Lab/02. Nums 1-10/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Nums_1_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = 0;
+            for (int i = 1; i <= 10; i++)
+            {
+                n = i;
+                Console.WriteLine(n);
+            }
+        }
+    }
+}
