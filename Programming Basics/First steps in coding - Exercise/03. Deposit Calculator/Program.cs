f0d8b2fb8a027376536ae5d34f79442b990356a5
IvanovvAlex <alexgeorgievivanov2003@gmail.com>
2020-12-18 06:56:53 +0200

    Add First Steps In Coding- Exercise

---
+++ Programming Basics/First steps in coding - Exercise/03. Deposit Calculator/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Deposit_Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double dep = double.Parse(Console.ReadLine());
+            int monts = int.Parse(Console.ReadLine());
+            double depro = double.Parse(Console.ReadLine());
+
+
+
+            Console.WriteLine(dep + (monts * ((dep * (depro / 100)) / 12)));
+        }
+    }
+}
---
+++ Programming Basics/First steps in coding - Exercise/07. Fruit Market/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Fruit_Market
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double priceKgStrawberry = double.Parse(Console.ReadLine());
+            double numBananas = double.Parse(Console.ReadLine());
+            double numOranges = double.Parse(Console.ReadLine());
+            double numRaspberries = double.Parse(Console.ReadLine());
+            double numStrawberry = double.Parse(Console.ReadLine());
+
+            double priceStrawberry = priceKgStrawberry;
+            double priceRaspberries = priceKgStrawberry / 2.0;
+            double priceOranges = priceRaspberries - (priceRaspberries * 0.40);
+            double priceBananas = priceRaspberries - (priceRaspberries * 0.80);
+
+            double totalPriceStrawberry = priceStrawberry * numStrawberry;
+            double totalPriceRaspberries = priceRaspberries * numRaspberries;
+            double totalPriceOranges = priceOranges * numOranges;
+            double totalPriceBananas = priceBananas * numBananas;
+
+            double total = totalPriceBananas + totalPriceOranges + totalPriceRaspberries + totalPriceStrawberry;
+            Console.WriteLine($"{total:f2}");
+        }
+    }
+}
