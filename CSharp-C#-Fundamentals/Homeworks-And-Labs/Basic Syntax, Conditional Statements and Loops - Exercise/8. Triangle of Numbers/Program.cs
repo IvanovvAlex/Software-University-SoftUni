a1b1b2ee5963c764e159743ccaac9fbb2e8fc587
IvanovvAlex <alexgeorgievivanov2003@gmail.com>
2021-01-03 21:14:03 +0200

    Add

---
+++ CSharp-C#-Fundamentals/Homeworks-And-Labs/Basic Syntax, Conditional Statements and Loops - Exercise/8. Triangle of Numbers/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _8._Triangle_of_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+
+            int start = 1;
+
+            for (int rows = 1; rows <= num; rows++)
+            {
+                
+                for (int cows = 1; cows <= start; cows++)
+                {
+                    Console.Write(start + " ");
+                    
+                }
+                Console.WriteLine();
+                start++;
+            }
+        }
+    }
+}
