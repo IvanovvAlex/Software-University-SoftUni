3560a142be824368fed8ba2fc7bab8bab3f38f6c
IvanovvAlex <alexgeorgievivanov2003@gmail.com>
2021-01-04 17:28:47 +0200

    Add

---
+++ CSharp-C#-Fundamentals/Homeworks-And-Labs/05. Arrays - Lab/04. Reverse Array of Strings/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace _04._Reverse_Array_of_Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string line = Console.ReadLine();
+            string[] elements = line
+                .Split()
+                .ToArray();
+
+            Console.Write(string.Join(' ', elements.Reverse()));
+
+        }
+    }
+}
---
+++ CSharp-C#-Fundamentals/Homeworks-And-Labs/05. Arrays - Lab/08. Condense Array to Number/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+namespace _08._Condense_Array_to_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numArr = Console.ReadLine()
+                .Split(' ')
+                .Select(int.Parse)
+                .ToArray();
+            if (numArr.Length == 1)
+            {
+                Console.WriteLine(numArr[0]);
+                return;
+            }
+            int finalResult = 0;
+            int firstLength = numArr.Length - 1;
+
+            for (int i = 0; i < firstLength; i++)
+            {
+                int[] condensedArr = new int[numArr.Length - 1];
+                for (int j = 0; j < condensedArr.Length; j++)
+                    condensedArr[j] = numArr[j] + numArr[j + 1];
+                numArr = condensedArr;
+                finalResult = condensedArr[0];
+            }
+            Console.WriteLine(finalResult);
+        }
+    }
+}
