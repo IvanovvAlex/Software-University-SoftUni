14b2a2349f5c2ba8ab2fc0568c0587faf24ed917
IvanovvAlex <alexgeorgievivanov2003@gmail.com>
2021-01-07 18:15:51 +0200

    Add

---
+++ CSharp-C#-Fundamentals/Mid-Exams/Mid Exam - 29 February 2020 Group 1/02. MuOnline/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Linq;
+
+namespace _02._MuOnline
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int health = 100;
+            int bitcoin = 0;
+
+            string[] rooms = Console.ReadLine()
+                .Split('|', StringSplitOptions.RemoveEmptyEntries)
+                .ToArray();
+
+            for (int i = 0; i < rooms.Length; i++)
+            {
+                string[] room = rooms[i]
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                    .ToArray();
+
+                switch (room[0].ToLower())
+                {
+                    case "potion":
+                        if (int.Parse(room[1]) + health <= 100)
+                        {
+                            health += int.Parse(room[1]);
+                            Console.WriteLine($"You healed for {room[1]} hp.");
+                            Console.WriteLine($"Current health: {health} hp.");
+                        }
+                        else
+                        {
+                            int diff = (health + int.Parse(room[1])) - 100;
+                            health = 100;                            
+                            Console.WriteLine($"You healed for {int.Parse(room[1]) - diff} hp.");
+                            Console.WriteLine($"Current health: {health} hp.");
+                        }
+                        break;
+                    case "chest":
+                        bitcoin += int.Parse(room[1]);
+                        Console.WriteLine($"You found {int.Parse(room[1])} bitcoins.");
+                        break;
+                    default:
+                        string monsterName = room[0];
+                        int damage = int.Parse(room[1]);
+
+                        health -= damage;
+
+                        if (health > 0)
+                        {
+                            Console.WriteLine($"You slayed {monsterName}.");
+                        }
+                        else
+                        {
+                            Console.WriteLine($"You died! Killed by {monsterName}.");
+                            Console.WriteLine($"Best room: {i+1}");
+                            return;
+                        }
+                        break; 
+                }
+            }
+
+            Console.WriteLine($"You've made it!");
+            Console.WriteLine($"Bitcoins: {bitcoin}");
+            Console.WriteLine($"Health: {health}");
+        }
+    }
+}
---
+++ CSharp-C#-Fundamentals/Mid-Exams/Mid Exam - 29 February 2020 Group 1/03. Inventory/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+
+namespace _03._Inventory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> list = Console.ReadLine()
+                .Split(", ")
+                .ToList();
+
+            string[] firstCommand = Console.ReadLine()
+                .Split(" - ")
+                .ToArray();
+
+            while (true)
+            {
+                string[] secondCommand = firstCommand;
+                switch (secondCommand[0])
+                {
+                    case "Collect":
+                        bool exist = false;
+                        for (int i = 0; i < list.Count; i++)
+                        {
+                            if (list[i] == secondCommand[1])
+                            {
+                                exist = true;
+                            }
+                        }
+                        if (exist == false)
+                        {
+                            list.Add(secondCommand[1]);
+                        }
+                        break;
+                    case "Drop":
+                        list.Remove(secondCommand[1]);
+                        break;
+
+                    case "Combine Items":
+                        string[] item = secondCommand[1]
+                            .Split(':')
+                            .ToArray();
+                        string oldItem = item[0];
+                        string newItem = item[1];
+
+                        for (int i = 0; i < list.Count; i++)
+                        {
+                            if (list[i] == oldItem)
+                            {
+                                list.Insert(i + 1, newItem);
+                            }
+                        }
+                        break;
+
+                    case "Renew":
+                        for (int i = 0; i < list.Count; i++)
+                        {
+                            if (list[i] == secondCommand[1])
+                            {
+                                list.RemoveAt(i);
+                                list.Add(secondCommand[1]);
+                            }
+                        }
+                        break;
+
+                    case "Craft!":
+                        Console.WriteLine(string.Join(", ", list));
+                        return;
+                }
+                firstCommand = Console.ReadLine()
+                .Split(" - ")
+                .ToArray();
+            }
+        }
+    }
+}
