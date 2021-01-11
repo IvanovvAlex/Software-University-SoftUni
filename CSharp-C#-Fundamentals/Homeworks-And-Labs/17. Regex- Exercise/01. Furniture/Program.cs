f6933cf405b36099105b5b170ae5642f4beb3036
IvanovvAlex <alexgeorgievivanov2003@gmail.com>
2021-01-11 19:10:12 +0200

    Add

---
+++ CSharp-C#-Fundamentals/Homeworks-And-Labs/17. Regex- Exercise/01. Furniture/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace _01._Furniture
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @">>(?<name>\w+)<<(?<price>\d+(.\d+)?)!(?<quant>\d+)";
+            string firstCommand = Console.ReadLine();
+
+            StringBuilder allProducts = new StringBuilder();
+
+            while (firstCommand != "Purchase")
+            {
+                string command = firstCommand;
+
+                allProducts.Append(command + ' ');
+
+                firstCommand = Console.ReadLine();
+            }
+           
+            MatchCollection buyedProducts = Regex.Matches(allProducts.ToString(), pattern);
+
+            Console.WriteLine("Bought furniture:");
+
+            decimal totalPrice = 0m;
+
+            foreach (Match item in buyedProducts)
+            {
+                Console.WriteLine($"{item.Groups["name"].Value}");
+
+                totalPrice += (decimal.Parse(item.Groups["price"].Value)
+                    * int.Parse(item.Groups["quant"].Value));
+            }
+            Console.WriteLine($"Total money spend: {totalPrice:f2}");
+        }
+    }
+}
---
+++ CSharp-C#-Fundamentals/Homeworks-And-Labs/17. Regex- Exercise/02. Race/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace _02._Race
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string namePattern = @"[A-Za-z]";
+            string distancePattern = @"[0-9]";
+
+            string[] names = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .ToArray();
+
+            Dictionary<string, int> statistic = new Dictionary<string, int>();
+
+            foreach (var name in names)
+            {
+                statistic.Add(name, 0);
+            }
+
+            string command = Console.ReadLine();
+
+            while (command != "end of race")
+            {
+                MatchCollection letters = Regex.Matches(command, namePattern);
+                StringBuilder nameBuild = new StringBuilder();
+                foreach (Match letter in letters)
+                {
+                    nameBuild.Append(letter.Value);
+                }
+                string name = nameBuild.ToString();
+                if (statistic.ContainsKey(name))
+                {
+                    MatchCollection digits = Regex.Matches(command, distancePattern);
+                    int distance = 0;
+                    foreach (Match meter in digits)
+                    {
+                        distance += int.Parse(meter.Value);
+                    }
+                    statistic[name] += distance;
+                }
+                command = Console.ReadLine();
+            }
+
+
+            string[] list = new string[] { "first", "second", "third" };
+            int i = 0;
+            foreach (KeyValuePair<string, int> people in statistic.OrderByDescending(key => key.Value))
+            {
+                list[i] = people.Key;
+                i++;
+                if (i == 3)
+                {
+                    Console.WriteLine($"1st place: {list[0]}");
+                    Console.WriteLine($"2nd place: {list[1]}");
+                    Console.WriteLine($"3rd place: {list[2]}");
+                    return;
+                }
+            }
+        }
+    }
+}
