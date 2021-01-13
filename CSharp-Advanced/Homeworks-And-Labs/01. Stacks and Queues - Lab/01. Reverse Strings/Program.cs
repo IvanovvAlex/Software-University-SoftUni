851eb15fbe52b2b65a0c447fdf0eb050053321a3
IvanovvAlex <alexgeorgievivanov2003@gmail.com>
2021-01-13 17:16:38 +0200

    Add

---
+++ CSharp-Advanced/Homeworks-And-Labs/01. Stacks and Queues - Lab/01. Reverse Strings/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01._Reverse_Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();            
+            Stack<char> stack = new Stack<char>(input);
+            while (stack.Count != 0)
+            {
+                Console.Write(stack.Pop());
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ CSharp-Advanced/Homeworks-And-Labs/01. Stacks and Queues - Lab/02. Stack Sum/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02._Stack_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            
+            Stack<int> stack = new Stack<int>(
+                input   
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray());
+
+            string[] command = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .ToArray();
+            while (command[0].ToLower() != "end")
+            {
+                switch (command[0].ToLower())
+                {
+                    case "add":
+                        stack.Push(int.Parse(command[1]));
+                        stack.Push(int.Parse(command[2]));
+                        break;
+                    case "remove":
+                        if (int.Parse(command[1]) <= stack.Count)
+                        {
+                            for (int i = 0; i < int.Parse(command[1]); i++)
+                            {
+                                stack.Pop();
+                            }
+                        }
+                        break;
+                }
+                command = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .ToArray();
+            }
+            Console.WriteLine($"Sum: {stack.Sum()}");
+        }
+    }
+}
