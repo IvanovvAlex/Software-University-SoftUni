5c0cc17881f7f6f94a575467d60d9d9b59611d52
IvanovvAlex <alexgeorgievivanov2003@gmail.com>
2021-01-13 17:58:24 +0200

    Add

---
+++ CSharp-Advanced/Homeworks-And-Labs/01. Stacks and Queues - Lab/03. Simple Calculator/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _03._Simple_Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries);             
+
+            Stack<string> stack = new Stack<string>(new Stack<string>(input));            
+
+           
+            while (stack.Count > 1)
+            {
+                int leftNum = int.Parse(stack.Pop());
+                string sign = stack.Pop();   
+                int rightNum = int.Parse(stack.Pop());
+
+                switch (sign)
+                {
+                    case "+":
+                        stack.Push((leftNum + rightNum).ToString());
+                        break;
+                    case "-":
+                        stack.Push((leftNum - rightNum).ToString());
+                        break;
+                }
+            }
+            Console.WriteLine(stack.Pop());
+        }
+    }
+}
