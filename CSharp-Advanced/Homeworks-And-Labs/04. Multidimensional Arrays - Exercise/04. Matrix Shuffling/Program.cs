07c9f30ab27bcaab9ea77cd83dd80ff0c7d1f2ea
IvanovvAlex <alexgeorgievivanov2003@gmail.com>
2021-01-17 17:12:07 +0200

    Add

---
+++ CSharp-Advanced/Homeworks-And-Labs/04. Multidimensional Arrays - Exercise/04. Matrix Shuffling/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace _04._Matrix_Shuffling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] firstLine = Console.ReadLine()
+               .Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+            int rows = int.Parse(firstLine[0]);
+            int cols = int.Parse(firstLine[1]);
+
+            string[,] matrix = new string[rows, cols];
+
+            for (int row = 0; row < rows; row++)
+            {
+                string[] line = Console.ReadLine()
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+                for (int col = 0; col < cols; col++)
+                {
+                    matrix[row, col] = line[col];
+                }
+            }
+
+            while (true)
+            {
+                string[] input = Console.ReadLine()
+               .Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+                if (input[0] == "END")
+                {
+                    break;
+                }
+
+                try
+                {
+                    int firstRow = int.Parse(input[1]);
+                    int firstCol = int.Parse(input[2]);
+                    int secondRow = int.Parse(input[3]);
+                    int secondCol = int.Parse(input[4]);
+
+                    string first = matrix[firstRow, firstCol];
+                    string second = matrix[secondRow, secondCol];
+
+                    matrix[firstRow, firstCol] = second;
+                    matrix[secondRow, secondCol] = first;
+
+                    for (int row = 0; row < rows; row++)
+                    {
+                        for (int col = 0; col < cols; col++)
+                        {
+                            Console.Write(matrix[row, col] + ' ');
+                        }
+                        Console.WriteLine();
+                    }
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Invalid input!");
+                }                
+            }
+        }
+    }
+}
---
+++ CSharp-Advanced/Homeworks-And-Labs/04. Multidimensional Arrays - Exercise/05. Snake Moves/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _05._Snake_Moves
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] firstLine = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+            int rows = int.Parse(firstLine[0]);
+            int cols = int.Parse(firstLine[1]);
+
+            char[,] matrix = new char[rows, cols];
+
+            string snake = Console.ReadLine();
+
+            Queue<char> queue = new Queue<char>();
+
+            for (int i = 0; i < snake.Length; i++)
+            {
+                queue.Enqueue(snake[i]);
+            }
+
+            for (int row = 0; row < rows; row++)
+            {
+                if (row % 2 == 0)
+                {
+                    for (int col = 0; col < cols; col++)
+                    {
+                        if (queue.Count == 0)
+                        {
+                            for (int i = 0; i < snake.Length; i++)
+                            {
+                                queue.Enqueue(snake[i]);
+                            }
+                        }
+
+                        matrix[row, col] = queue.Dequeue();
+                    }
+                    
+                }
+                else
+                {
+                    for (int col = cols - 1; col >= 0; col--)
+                    {
+                        if (queue.Count == 0)
+                        {
+                            for (int i = 0; i < snake.Length; i++)
+                            {
+                                queue.Enqueue(snake[i]);
+                            }
+                        }
+                        matrix[row, col] = queue.Dequeue();
+                    }                    
+                }
+            }
+
+            for (int row = 0; row < rows; row++)
+            {
+                for (int col = 0; col < cols; col++)
+                {
+                    Console.Write(matrix[row, col]);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
