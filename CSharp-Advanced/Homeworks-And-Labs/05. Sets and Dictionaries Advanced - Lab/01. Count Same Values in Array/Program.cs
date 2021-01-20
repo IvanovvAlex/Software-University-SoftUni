c01a06f36e17d9fd7993d1b47aca40a7144b3821
IvanovvAlex <alexgeorgievivanov2003@gmail.com>
2021-01-20 19:11:52 +0200

    Add

---
+++ CSharp-Advanced/Homeworks-And-Labs/05. Sets and Dictionaries Advanced - Lab/01. Count Same Values in Array/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01._Count_Same_Values_in_Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] collection = Console.ReadLine()
+                .Split()
+                .Select(double.Parse)
+                .ToArray();
+
+            Dictionary<double, double> result = new Dictionary<double, double>();
+
+            foreach (var item in collection)
+            {
+                if (!result.ContainsKey(item))
+                {
+                    result.Add(item, 0);
+                }
+                result[item]++;
+            }
+
+            foreach (var item in result)
+            {
+                Console.WriteLine($"{item.Key} - {item.Value} times");
+            }
+        }
+    }
+}
---
+++ CSharp-Advanced/Homeworks-And-Labs/05. Sets and Dictionaries Advanced - Lab/02. Average Student Grades/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace _02._Average_Student_Grades
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Dictionary<string, List<decimal>> info = new Dictionary<string, List<decimal>>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] input = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                if (!info.ContainsKey(input[0]))
+                {
+                    info.Add(input[0], new List<decimal>());
+                }
+
+                info[input[0]].Add(decimal.Parse(input[1]));
+            }
+
+            foreach (var people in info)
+            {
+                Console.Write($"{people.Key} -> ");
+                foreach (var item in people.Value)
+                {
+                    Console.Write($"{item:f2} ");
+                }
+                Console.Write($"(avg: {(people.Value.Sum() / people.Value.Count):f2})");
+                Console.WriteLine();
+            }
+        }
+    }
+}
