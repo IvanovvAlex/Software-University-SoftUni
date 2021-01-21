24bf494573b00c7e216cb26344851bc11c2714f5
IvanovvAlex <alexgeorgievivanov2003@gmail.com>
2021-01-21 18:38:11 +0200

    Add

---
+++ CSharp-Advanced/Homeworks-And-Labs/06. Sets and Dictionaries Advanced - Exercise/05. Count Symbols/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _05._Count_Symbols
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string line = Console.ReadLine();
+
+            SortedDictionary<char, int> info = new SortedDictionary<char, int>();
+
+            for (int i = 0; i < line.Length; i++)
+            {
+                
+                if (!info.ContainsKey(line[i]))
+                {
+                    info.Add(line[i], 0);
+                }
+                info[line[i]]++;
+            }
+
+            foreach (var item in info)
+            {
+                Console.WriteLine($"{item.Key}: {item.Value} time/s");
+            }
+        }
+    }
+}
---
+++ CSharp-Advanced/Homeworks-And-Labs/06. Sets and Dictionaries Advanced - Exercise/06. Wardrobe/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace _06._Wardrobe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Dictionary<string, Dictionary<string, int>> wardrobe = 
+                new Dictionary<string, Dictionary<string, int>>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] firstArr = Console.ReadLine()
+                    .Split("->", StringSplitOptions.RemoveEmptyEntries);
+
+                string color = firstArr[0].Substring(0, firstArr[0].Length-1);
+
+                string[] arrWithClothes = firstArr[1]
+                    .Split(",", StringSplitOptions.RemoveEmptyEntries);
+
+                arrWithClothes[0] = arrWithClothes[0].Substring(1);
+
+                if (!wardrobe.ContainsKey(color))
+                {
+                    wardrobe.Add(color, new Dictionary<string, int>());
+                }
+
+                foreach (var cloth in arrWithClothes)
+                {
+                    if (!wardrobe[color].ContainsKey(cloth))
+                    {
+                        wardrobe[color].Add(cloth, 0);
+                    }
+                    wardrobe[color][cloth]++;
+                }
+            }
+
+            string[] lastLine = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            string colorSearch = lastLine[0];
+            string clothSearch = lastLine[1];
+
+            foreach (var color in wardrobe)
+            {
+                Console.WriteLine($"{color.Key} clothes:");
+                foreach (var item in color.Value)
+                {
+                    if (item.Key == clothSearch && color.Key == colorSearch)
+                    {
+                        Console.WriteLine($"* {item.Key} - {item.Value} (found!)");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"* {item.Key} - {item.Value}");
+                    }
+                }
+            }
+        }
+    }
+}
