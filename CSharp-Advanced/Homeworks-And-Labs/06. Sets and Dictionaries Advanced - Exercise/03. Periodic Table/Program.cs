3c9a5fd15fbb8e988b92bf355b3d1f2a0229c29e
IvanovvAlex <alexgeorgievivanov2003@gmail.com>
2021-01-21 18:04:15 +0200

    Add

---
+++ CSharp-Advanced/Homeworks-And-Labs/06. Sets and Dictionaries Advanced - Exercise/03. Periodic Table/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _03._Periodic_Table
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            List<string> list = new List<string>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] line = Console.ReadLine()
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                for (int j = 0; j < line.Length; j++)
+                {
+                    if (!list.Contains(line[j]))
+                    {
+                        list.Add(line[j]);
+                    }
+                }
+            }
+
+            foreach (var item in list.OrderBy(n => n))
+            {
+                Console.Write($"{item} ");
+            }
+        }
+    }
+}
---
+++ CSharp-Advanced/Homeworks-And-Labs/06. Sets and Dictionaries Advanced - Exercise/04. Even Times/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace _04._Even_Times
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Dictionary<int, int> info = new Dictionary<int, int>();
+
+            for (int i = 0; i < n; i++)
+            {
+                int num = int.Parse(Console.ReadLine());
+                if (!info.ContainsKey(num))
+                {
+                    info.Add(num, 0);
+                }
+                info[num]++;
+            }
+
+            foreach (var num in info)
+            {
+                if (num.Value % 2 == 0)
+                {
+                    Console.WriteLine(num.Key);
+                    break;
+                }
+            }
+        }
+    }
+}
