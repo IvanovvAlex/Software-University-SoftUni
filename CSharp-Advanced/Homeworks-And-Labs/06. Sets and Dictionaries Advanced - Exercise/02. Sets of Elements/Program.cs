155ddf2206e1bd22ecc2aae7885d9890d3a7ccd8
IvanovvAlex <alexgeorgievivanov2003@gmail.com>
2021-01-21 17:51:08 +0200

    Add

---
+++ CSharp-Advanced/Homeworks-And-Labs/06. Sets and Dictionaries Advanced - Exercise/02. Sets of Elements/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02._Sets_of_Elements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] n = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            int firstLength = n[0];
+            int secondLength = n[1];
+
+            HashSet<int> firstSet = new HashSet<int>();
+
+            for (int i = 0; i < firstLength; i++)
+            {
+                int add = int.Parse(Console.ReadLine());
+                firstSet.Add(add);
+            }
+
+            HashSet<int> secondSet = new HashSet<int>();
+
+            for (int i = 0; i < secondLength; i++)
+            {
+                int add = int.Parse(Console.ReadLine());
+                secondSet.Add(add);
+            }
+
+            HashSet<int> resultSet = new HashSet<int>();
+
+            foreach (var item in firstSet)
+            {
+                if (secondSet.Contains(item))
+                {
+                    resultSet.Add(item);
+                }
+            }
+
+            Console.WriteLine(string.Join(' ', resultSet));
+        }
+    }
+}
