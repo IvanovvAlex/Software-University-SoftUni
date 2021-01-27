e64b873ede228099ac31f162078cfba94f85bb22
IvanovvAlex <alexgeorgievivanov2003@gmail.com>
2021-01-27 18:46:56 +0200

    Add

---
+++ CSharp-Advanced/Homeworks-And-Labs/09. Functional Programming - Lab/03. Count Uppercase Words/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _03._Count_Uppercase_Words
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<string, bool> func = IsUppercase;
+
+            List<string> input = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Where(x => func(x))
+                .ToList();
+
+            input.ForEach(x => Console.WriteLine(x));
+        }
+
+        static bool IsUppercase(string word)
+        {
+            byte[] asciiBytes = Encoding.ASCII.GetBytes(word);
+            if (asciiBytes[0] >= 65 && asciiBytes[0] <= 90)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ CSharp-Advanced/Homeworks-And-Labs/09. Functional Programming - Lab/04. Add VAT/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _04._Add_VAT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<decimal> prices = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(decimal.Parse)
+                .Select(x => x += (x * 0.2m))
+                .ToList();
+
+            prices.ForEach(x => Console.WriteLine($"{x:f2}"));
+        }
+    }
+}
