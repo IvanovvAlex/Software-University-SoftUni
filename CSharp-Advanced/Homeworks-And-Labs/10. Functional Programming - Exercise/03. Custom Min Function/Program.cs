c731ce729ecffadc9eb6c0380f4249242fae9fe4
IvanovvAlex <alexgeorgievivanov2003@gmail.com>
2021-01-28 13:19:30 +0200

    Add

---
+++ CSharp-Advanced/Homeworks-And-Labs/10. Functional Programming - Exercise/03. Custom Min Function/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+
+namespace _03._Custom_Min_Function
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            Func<int[], int> func = Min;
+            Console.WriteLine(func(arr));
+        }
+
+        static int Min(int[] arr)
+        {
+            int min = int.MaxValue;
+            foreach (var item in arr)
+            {
+                if (item < min)
+                {
+                    min = item;
+                }
+            }
+            return min;
+        }
+    }
+}
---
+++ CSharp-Advanced/Homeworks-And-Labs/10. Functional Programming - Exercise/04. Find Evens or Odds/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04._Find_Evens_or_Odds
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = Console.ReadLine()
+                 .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                 .Select(int.Parse)
+                 .ToArray();
+            int min = arr[0];
+            int max = arr[1];
+
+            List<int> list = new List<int>();
+
+            for (int i = min; i <= max; i++)
+            {
+                list.Add(i);
+            }
+
+            string evenOrOdd = Console.ReadLine();
+
+            Func<int, bool> func = Filter(evenOrOdd);
+
+            list = list
+                .Where(func)
+                .ToList();
+            Console.WriteLine(string.Join(" ", list));
+        }
+
+        private static Func<int, bool> Filter(string evenOrOdd)
+        {
+            switch (evenOrOdd)
+            {
+                case "even":
+                    return x => x % 2 == 0;
+                case "odd":
+                    return x => x % 2 != 0;
+                default:
+                    return x => true;                   
+            }
+        }
+    }
+}
