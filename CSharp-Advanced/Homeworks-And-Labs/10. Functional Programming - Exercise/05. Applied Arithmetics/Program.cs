18a67a52e63ea2152fc82eca396a10db614b77b1
IvanovvAlex <alexgeorgievivanov2003@gmail.com>
2021-01-28 14:05:08 +0200

    Add

---
+++ CSharp-Advanced/Homeworks-And-Labs/10. Functional Programming - Exercise/05. Applied Arithmetics/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Linq;
+
+namespace _05._Applied_Arithmetics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            string command = Console.ReadLine();
+            while (command != "end")
+            {
+                if (command != "print")
+                {
+                    Func<int, int> func = Filter(command);
+                    arr = arr
+                        .Select(func)
+                        .ToArray();
+                }
+                else
+                {
+                    Console.WriteLine(string.Join(" ", arr));
+                }
+                
+                command = Console.ReadLine();
+            }            
+        }
+
+        static Func<int, int> Filter(string command)
+        {
+            switch (command)
+            {
+                case "add":
+                    return x => x + 1;                    
+                case "multiply":
+                    return x => x * 2;
+                case "subtract":
+                    return x => x - 1;                
+                default:
+                    return x => x;
+            }
+        }
+    }
+}
---
+++ CSharp-Advanced/Homeworks-And-Labs/10. Functional Programming - Exercise/06. Reverse And Exclude/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+
+namespace _06._Reverse_And_Exclude
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = Console.ReadLine()
+               .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+               .Select(int.Parse)
+               .ToArray();
+
+            int n = int.Parse(Console.ReadLine());
+
+            Func<int[], int[]> rev = Reverser;
+            arr = rev(arr);
+            
+            arr = arr
+                .Where(x => x % n != 0)
+                .ToArray();
+            Console.WriteLine(string.Join(" ", arr));
+        }
+
+       
+
+        private static int[] Reverser(int[] array)
+        {
+            int holder = 0;
+            int size = array.Length;
+            for (int i = 0; i < size / 2; ++i)
+            {
+                holder = array[i];
+                array[i] = array[size - 1 - i];
+                array[size - 1 - i] = holder;
+            }
+            return array;
+        }
+    }
+}
