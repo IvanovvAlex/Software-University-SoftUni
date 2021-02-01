5d68b17770fb6515783923a9ffca8d96c0d7589f
IvanovvAlex <alexgeorgievivanov2003@gmail.com>
2021-02-01 16:14:51 +0200

    Add

---
+++ CSharp-Advanced/Homeworks-And-Labs/12. Defining Classes - Exercise/03.OldestFamilyMember/Family.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DefiningClasses
+{
+    public class Family
+    {
+        public List<Person> People { get; set; } = new List<Person>();
+
+        public void AddMember(Person member) 
+        {
+            People.Add(member);
+        }
+
+        public Person GetOldestMember()
+        {
+            return People.OrderByDescending(x => x.Age).FirstOrDefault();
+        }
+    }
+}
