509cbd6bd3d1f5ef31ef228549309aff82945367
IvanovvAlex <alexgeorgievivanov2003@gmail.com>
2021-02-15 09:13:00 +0200

    Add

--- CSharp-Advanced/Homeworks-And-Labs/12. Defining Classes - Exercise/09PokemonTrainer/Pokemon.cs
+++ CSharp-Advanced/Homeworks-And-Labs/12. Defining Classes - Exercise/09PokemonTrainer/Pokemon.cs
@@ -1,14 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace DefiningClasses
+﻿namespace DefiningClasses
 {
+    using System;
+
     public class Pokemon
     {
         public string Name { get; set; }
+
         public string Element { get; set; }
+
         public int Health { get; set; }
+
         public Pokemon(string name, string element, int health)
         {
             this.Name = name;
