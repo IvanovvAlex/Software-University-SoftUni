15f2baaf2d8294bc9116cd05de183c14af14d479
Alex Ivanov <alexivanovv4@gmail.com>
2021-02-18 23:59:18 +0200

    Add

---
+++ CSharp-Advanced/Exams/Exam - 19 August 2020/Ali/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Ali
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> vanka = new List<int>();
+            vanka.Add(5);
+            vanka.Remove(5);
+            Console.WriteLine(string.Join(" ", vanka));
+
+        }
+    }
+}
