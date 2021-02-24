8bd328e32ee0da8a2b0d6f62fe3e3da35bfb7981
Alex Ivanov <alexivanovv4@gmail.com>
2021-02-24 13:10:45 +0200

    Edit

---
+++ CSharp-OOP/Homeworks-And-Labs/01.InheritanceLab/05StackOfStrings/StackOfStrings.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _05StackOfStrings
+{
+    public class StackOfStrings : Stack<string>
+    {
+        public bool IsEmpty()
+        {
+            if (this.Count == 0)
+            {
+                return true;
+            }
+            return false;
+        }
+        public Stack<string> AddRange(IEnumerable<string> range)
+        {
+            foreach (var item in range)
+            {
+                this.Push(item);
+            }
+            return this;
+        }
+    }
+}
