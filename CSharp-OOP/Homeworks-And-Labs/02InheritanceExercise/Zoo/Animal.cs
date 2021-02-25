f7ad17e8996144e741cf590255aea28be264ed01
Alex Ivanov <alexivanovv4@gmail.com>
2021-02-25 09:43:01 +0200

    Add

---
+++ CSharp-OOP/Homeworks-And-Labs/02InheritanceExercise/Zoo/Animal.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Zoo
+{
+    public class Animal
+    {
+        private string name;
+        public Animal(string name)
+        {
+            Name = name;
+        }
+        public string Name { get; set; }
+    }
+}
