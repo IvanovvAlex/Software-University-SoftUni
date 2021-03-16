9200ab811d3ab641688851774bb01222bef9f043
IvanovvAlex <alexgeorgievivanov2003@gmail.com>
2021-03-16 11:02:45 +0200

    Add

---
+++ CSharp-OOP/Homeworks-And-Labs/02InheritanceExercise/NeedForSpeed/Car.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NeedForSpeed
+{
+    public class Car : Vehicle
+    {
+        public Car(int horsePower, double fuel)
+          : base(horsePower, fuel)
+        {
+
+        }
+
+        private double defaultFuelConsumption = 3;
+
+        public override double FuelConsumption
+        {
+            get { return defaultFuelConsumption; }
+            set { FuelConsumption = defaultFuelConsumption; }
+        }
+
+        public override void Drive(double kilometers)
+        {
+            Fuel -= kilometers * FuelConsumption;
+        }
+    }
+}
---
+++ CSharp-OOP/Homeworks-And-Labs/02InheritanceExercise/NeedForSpeed/RaceMotorcycle.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NeedForSpeed
+{
+    public class RaceMotorcycle : Motorcycle
+    {
+        public RaceMotorcycle(int horsePower, double fuel)
+           : base(horsePower, fuel)
+        {
+
+        }
+        private double defaultFuelConsumption = 8;
+
+        public override double FuelConsumption
+        {
+            get { return defaultFuelConsumption; }
+            set { FuelConsumption = defaultFuelConsumption; }
+        }
+        public override void Drive(double kilometers)
+        {
+            Fuel -= kilometers * FuelConsumption;
+        }
+    }
+}
